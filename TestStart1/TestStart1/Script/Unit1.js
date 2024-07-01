fd766d66d00b21887e03d5ebab4f32de8a3ff128
Gokul8008 <sgokul0710@gmail.com>
2024-07-01 06:45:31 +0000

    TestCommit

---
+++ TestStart1/TestStart1/Script/Unit1.js
@@ -0,0 +1,32 @@
+﻿function FindChildM(PropArray, ValuesArray, PObject, Depth, Click, Keys, text) {
+
+
+    //var PropNArray = new Array(PropArray);
+    //var ValuesNArray = new Array(ValuesArray);
+    Delay(2000);
+    Sys.Refresh();
+    Aliases.Skype.Refresh();
+    Aliases.Skype.RefreshMappingInfo();
+    
+    var w = PObject.FindChild(PropArray, ValuesArray, Depth);
+
+    // Processes the search results
+    if (w.Exists != false) {
+
+        // Log.Message("Object Found successfully");
+        Log.Checkpoint("Button Found successfully");
+        //Log.Message("Button found successfully " + w.WndCaption)
+        Sys.Refresh();
+        Aliases.Skype.Refresh();
+        Aliases.Skype.RefreshMappingInfo();
+        if(Click==true){
+          w.Click();
+        }
+        if(Keys==true){
+          w.Keys(text);
+        }
+        return true;
+
+    } else
+        Log.Error("The object was not found.");
+}
